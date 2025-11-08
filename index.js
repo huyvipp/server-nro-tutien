7f842c542e1116fddfe8b9f01a6a2d143fa0afc9
huyvipp <118251131+huyvipp@users.noreply.github.com>
2025-11-08 20:44:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ app.get('/', (req, res) => {
   res.send('NodeJS server is running 🚀');
 });
 
-app.listen(8080, () => console.log('Server running on port 8080'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
